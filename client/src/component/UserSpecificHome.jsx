8a173eda386e51da678c56d29819e6164b2afae2
cmabdulkareem <cmabdulkareem@gmail.com>
2024-05-06 21:21:50 +0530

    Update UserSpecificHome.jsx

--- client/src/component/UserSpecificHome.jsx
+++ client/src/component/UserSpecificHome.jsx
@@ -9,7 +9,7 @@ function UserSpecificPage(){
 
 
     useEffect(() => {
-        axios.get('https://sample-reactapp.vercel.app', { withCredentials: true })
+        axios.get('http://localhost:3000/', { withCredentials: true })
             .then((result) => {
                 if(result.data.Valid){
                     setName(result.data.username)
