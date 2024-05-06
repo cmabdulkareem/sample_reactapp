89675cd47c7a6195eafa9ee0f92787c26f7961d2
cmabdulkareem <cmabdulkareem@gmail.com>
2024-05-06 18:26:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['sample-reactapp.vercel.app'],
     methods: ["POST", "GET"], // Adjust the port to match your React app's port
     credentials: true
 }));
