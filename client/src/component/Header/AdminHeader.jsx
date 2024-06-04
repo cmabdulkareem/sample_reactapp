1c1bd567427ac38fc95ae1c0d9aad45b00eb726a
cmabdulkareem <cmabdulkareem@gmail.com>
2024-06-04 06:57:37 +0530

    added search and filter

--- client/src/component/Header/AdminHeader.jsx
+++ client/src/component/Header/AdminHeader.jsx
@@ -1,4 +1,20 @@
-function AdminHeader() {
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+function AdminHeader(props) {
+
+    const navigate = useNavigate()
+
+    function handleLogout(){
+        axios.get('/api/logout')
+            .then((result)=>{
+                if(result.data === "logoutsuccess"){
+                    navigate('/register')
+                }
+            })
+        }
+
     return (
         <>
             <header className="p-3 mb-3 border-bottom">
@@ -10,18 +26,25 @@ function AdminHeader() {
                         </a>
 
                         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                            <li><a href="#" className="nav-link px-2 link-body-emphasis">Products</a></li>
-                            <li><a href="#" className="nav-link px-2 link-body-emphasis">Cart</a></li>
-                            <li><a href="#" className="nav-link px-2 link-body-emphasis">My Order</a></li>
+                            <li><Link to="/" className="nav-link px-2 link-body-emphasis">Products</Link></li>
+                            <li><Link to="/orders" className="nav-link px-2 link-body-emphasis">Orders</Link></li>
+                            <li><Link to="/settings" className="nav-link px-2 link-body-emphasis">Settings</Link></li>
                         </ul>
 
                         <div className="dropdown text-end me-4">
                             <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                                 <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
                             </a>
+                            
                             <ul className="dropdown-menu text-small" style={{}}>
-                                <li><a className="dropdown-item" href="#">Create Account</a></li>
-                                <li><a className="dropdown-item" href="#">Login</a></li>
+                                
+
+                            {props.isLoggedIn ? 
+                                (<li><a className="dropdown-item" onClick={handleLogout}>Logout</a></li>
+                            ):(
+                                <div><li><a className="dropdown-item" href="#">Create Account</a></li>
+                                <li><a className="dropdown-item" href="#">Login</a></li></div>)}
+                                
                                 <li><hr className="dropdown-divider" /></li>
                                 <li><a className="dropdown-item" href="#">Help</a></li>
                             </ul>
@@ -41,3 +64,5 @@ function AdminHeader() {
 }
 
 export default AdminHeader;
+
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,10 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 //Importing bootstrap JavaScript bundle
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>,
 )
